5ae8600c12909827ff31570ef433fe1642c96a7e
Alexandre Ribault (Capgemini) <alexandre.ribault@capgemini.com>
2023-03-29 11:19:08 +0200

    Installation of react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import AddBooks from "./containers/AddBooks";
+import SearchBooks from "./containers/SearchBooks";
+import Error from "./components/Error";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
+        <BrowserRouter>
             <NavBar />
-            <AddBooks />
+
+            <Routes>
+                <Route exact path="/" element={<AddBooks />} />
+                <Route path="/search" element={<SearchBooks />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+
             <Footer />
-        </div>
+        </BrowserRouter>
     );
 }
 
